350cb4b46d7fa2444fe0ba55e74cdd67809e89d3
pashalahh <ramdinpasha@gmail.com>
2025-10-01 11:55:05 +0700

    loginform

---
+++ Documents/NetBeansProjects/MINUS1/src/main/java/com/mycompany/minus1/MINUS1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.minus1;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class MINUS1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
